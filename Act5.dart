90eec98dab731189832eeffbedb26d92ee3ba10f
Trisha Nicole Almario <trishanicolealmario04@gmail.com>
2025-08-15 18:01:39 +0800

    Update Act5.dart

--- Act5.dart
+++ Act5.dart
@@ -1,1 +1,29 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Please input your age: ");
+  String? input = stdin.readLineSync();
+
+  if (input != null && input.isNotEmpty) {
+    var age = int.tryParse(input);
+
+    if (age != null) {
+      String status = (age >= 18) ? "Adult" : "Minor";
+      print("Status: $status");
+    } else {
+      print("Invalid input. Please enter a valid number.");
+    }
+  } else {
+    print("No input received.");
+  }
+}
+
+Output:
+  
+Please input your age: 20
+Status: Adult
+
+Please input your age: 14
+Status: Minor
+
 
